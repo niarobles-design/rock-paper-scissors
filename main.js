c7caae94dc57c98ee09b467937e71384dbf75679
Estefanía Robles <l02rosee@gmail.com>
2025-02-20 11:42:33 +0100

    feat: function that returns the player choice

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 //If 'result' = 2, the function returns 'scissors'
 
 let computerChoice = "";
-
+let humanChoice = "";
 function getComputerChoice(max) {
   let result = Math.floor(Math.random() * max);
   if (result === 0) {
@@ -15,3 +15,31 @@ function getComputerChoice(max) {
     return "scissors";
   }
 }
+
+//The function ask the user to enter input and it assigns it to the 'result' variable
+//Then it converts it to lowercase and checks if it matches:
+//In case the input doesn't match: 'rock', 'paper' or 'scissors'
+//It keeps asking for a correct input until it gets a correct one
+//Finally it returns 'result'
+
+function getHumanChoice() {
+  let result = prompt(
+    "What's your choice? 'Rock', 'paper', or scissors?"
+  ).toLocaleLowerCase();
+
+  while (result !== "rock" || result !== "paper" || result !== "scissors") {
+    result = prompt(
+      "Invalid input, please enter: 'rock', 'paper', or scissors"
+    ).toLocaleLowerCase();
+    if (result === "rock" || result === "paper" || result === "scissors") {
+      break;
+    }
+  }
+  return result;
+}
+
+computerChoice = getComputerChoice(3);
+console.log(computerChoice);
+
+humanChoice = getHumanChoice();
+console.log(humanChoice);
